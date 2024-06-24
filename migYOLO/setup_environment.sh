e6517d51a3bfe4578c9fc12776ee8e1e338b904c
Jeff Schueler <jschuel@hawaii.edu>
2024-06-24 16:56:54 -0700

    setupscript now pulls correct data

--- migYOLO/setup_environment.sh
+++ migYOLO/setup_environment.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# Function to download files using wget
+download_files() {
+    for i in {1..5}; do
+	wget "https://lobogit.unm.edu/jschueler1/migYOLO/-/raw/main/migYOLO/data_and_models${i}.zip"
+    done
+}
+
 # Function to unzip the given file
 unzip_directory() {
     local zip_file_path=$1
@@ -9,10 +16,11 @@ unzip_directory() {
 
 # Function to make directories
 makedirs() {
-    local paths=("data/benchmark/random_benchmark_image/"
+    local paths=("data/"
+		 "data/benchmark/random_benchmark_image/"
                  "data/benchmark/high_occupancy_image/"
                  "data/dark/"
-		 "data/raw_images/"
+                 "data/raw_images/"
                  "models/")
     for path in "${paths[@]}"; do
         if [ ! -d "$path" ]; then
@@ -23,31 +31,36 @@ makedirs() {
 
 # Main function
 main() {
-    local zip_file_path="data_and_models.zip"
-    local extract_to=""
+    echo "Downloading zip files"
+    download_files
+    echo "Done!"
 
-    if [ ! -f "$zip_file_path" ]; then
-        echo "Script will not run unless there is a data_and_models.zip file to unzip!"
-        exit 1
-    fi
-
-    echo "Unzipping data_and_models.zip"
-    unzip_directory "$zip_file_path" "$extract_to"
+    echo "Unzipping files"
+    for i in {1..5}; do
+        unzip_directory "data_and_models${i}.zip" ""
+    done
     echo "Done!"
 
     echo "Making relevant directories"
     makedirs
 
-    echo "Moving files"
-    mv "zipped_files/random_benchmark_image/random.MTIFF" "data/benchmark/random_benchmark_image/random.MTIFF"
-    mv "zipped_files/high_occupancy_image/high_occupancy.MTIFF" "data/benchmark/high_occupancy_image/high_occupancy.MTIFF"
-    mv "zipped_files/dark/sample_master_dark.npy" "data/dark/sample_master_dark.npy"
-    mv zipped_files/raw_images/* data/raw_images/
-    mv zipped_files/models/* models/
+    echo "Moving files from data_and_models1.zip"
+    mv zipped_files1/benchmark/random_benchmark_image/random.MTIFF data/benchmark/random_benchmark_image/random.MTIFF
+    mv zipped_files1/benchmark/high_occupancy_image/high_occupancy.MTIFF data/benchmark/high_occupancy_image/high_occupancy.MTIFF
+    mv zipped_files1/dark/sample_master_dark.npy data/dark/sample_master_dark.npy
+    
+    echo "Moving files from data_and_models[1-5].zip"
+    for i in {2..5}; do
+        mv "zipped_files${i}/raw_images"/* "data/raw_images/"
+    done
+    
+    mv zipped_files1/models/* models/
     
     echo "Cleaning up"
-    rm "$zip_file_path"
-    rm -rf "zipped_files/"
+    for i in {1..5}; do
+        rm "data_and_models${i}.zip"
+	rm -rf "zipped_files${i}/"
+    done
 
     echo "SUCCESS!"
 }
